ea149f000b232eb8294e3680edb4aab8997a997b
Julia Branke <Julia.Branke@gmx.co.uk>
2022-04-27 11:02:32 +0100

    activity 2

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -2,20 +2,29 @@ import { DisneyCharacter } from "../disney_character"
 
 // for our props we can reuse the DisneyCharacter interface
 // - defining an anonymous type that just has one property - a DisneyCharacter
-const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => 
-  
-    <article className="character-item">
+const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => {
+    // Define a default in case the character doesn't have an image
+    let imageSrc = "https://picsum.photos/300/200/?blur";
+    if (character.imageUrl)
+    {
+        // API seems to include extra path for images so here we strip it off to fetch raw image
+        imageSrc = character.imageUrl.substring(0, character.imageUrl.indexOf('/revision'));
+        imageSrc = character.imageUrl;
 
-      <h2>{character.name}</h2>
+    }
+    return (
+        <article className="character-item">
 
-      <div className="character-item__actions">
-        Add to Favourites
-      </div>
+            <h2>{character.name}</h2>
 
-      <img className="character-item__img" src={character.imageUrl} alt={character.name} />
+            <div className="character-item__actions">
+                Add to Favourites
+            </div>
 
-    </article>
-  
+            <img className="character-item__img" src={imageSrc} alt={character.name} />
 
+        </article>
+        )
+}
 
 export default Character
